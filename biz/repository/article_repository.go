1a081b8715fa26ea9a4d0ff3151a29d20a37d213
惊蛰 <475437183@qq.com>
2025-01-18 02:16:43 +0800

    初始化代码

--- biz/repository/article_repository.go
+++ biz/repository/article_repository.go
@@ -1,5 +1,9 @@
 package repository
 
+import "plugin-sdk/biz/model"
+
 type ArticleRepository interface {
 	UseArticle(articleId string) error
+	GetArticles(name string) ([]*model.Article, error)
+	GetArticleDetail(articleId string) model.Article
 }
