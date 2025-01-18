6a87c60e6a47a0e057357468857b8783849adddf
惊蛰 <475437183@qq.com>
2025-01-18 12:11:54 +0800

    初始化代码

---
+++ plugin-sdk/test/article_test.go
@@ -0,0 +1,20 @@
+package test
+
+import (
+	plugin_sdk "github.com/xieyaoxin/pockpluginsdk/plugin-sdk"
+	"github.com/xieyaoxin/pockpluginsdk/plugin-sdk/biz/log"
+	"testing"
+)
+
+func TestQueryArticleList(t *testing.T) {
+	User := GetLoginUser()
+	plugin_sdk.UserServiceInstance.Login(User.LoginName, User.Password)
+	articleList, err := plugin_sdk.ArticleServiceInstance.QueryArticleList("")
+	if err != nil {
+		return
+	}
+	for _, article := range articleList {
+		log.Info("获取物品列表 %v", article)
+
+	}
+}
